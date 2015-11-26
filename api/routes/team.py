b8300b01b7ec3a9a2b19945d1c83c8be9189a01e
Christopher Ganas <cganas@forallsecure.com>
2015-11-25 23:15:56 -0500

    Moved team routing code into routes/team.py

--- api/routes/team.py
+++ api/routes/team.py
@@ -7,6 +7,9 @@ from api.annotations import api_wrapper, require_login, require_teacher, require
 from api.annotations import block_before_competition, block_after_competition
 from api.annotations import log_action
 
+from api.common import check, validate, safe_fail
+from voluptuous import Required, Length, Schema
+
 blueprint = Blueprint("team_api", __name__)
 
 @blueprint.route('', methods=['GET'])
@@ -24,19 +27,71 @@ def get_team_score_hook():
         return WebSuccess(data={'score': score})
     return WebError("There was an error retrieving your score.")
 
+new_team_schema = Schema({
+    Required("team_name"): check(
+        ("The team name must be between 3 and 40 characters.", [str, Length(min=3, max=40)]),
+        ("A team with that name already exists.", [
+            lambda name: safe_fail(api.team.get_team, name=name) is None]),
+        ("A username with that name already exists.", [
+            lambda name: safe_fail(api.user.get_user, name=name) is None]),
+    ),
+    Required("team_password"): check(
+        ("Passwords must be between 3 and 20 characters.", [str, Length(min=3, max=20)]))
+}, extra=True)
 
 @blueprint.route('/create', methods=['POST'])
 @api_wrapper
 @require_login
 def create_new_team_hook():
-    api.team.create_new_team_request(api.common.flat_multi(request.form))
+    """
+    Fulfills new team requests for users who have already registered.
+
+    Args:
+        team_name: The desired name for the team. Must be unique across users and teams.
+        team_password: The team's password.
+    Returns:
+        True if successful, exception thrown elsewise. 
+    """
+
+    params = api.common.flat_multi(request.form)
+    validate(new_team_schema, params)
+
+    user = api.user.get_user(uid=uid)
+    current_team = api.team.get_team(tid=user["tid"])
+
+    if current_team["team_name"] != user["username"]:
+        raise InternalException("You can only create one new team per user account!")
+
+    desired_tid = create_team({
+        "team_name": params["team_name"],
+        "password": params["team_password"],
+        # The team's affiliation becomes the creator's affiliation.
+        "affiliation": current_team["affiliation"],
+        "eligible": True
+    })
+
+    api.team.join_team(params["team_name"], params["team_password"], user["uid"])
+
     return WebSuccess("You now belong to your newly created team.")
+
+join_team_schema = Schema({
+    Required("team_name"): check(
+        ("The team name must be between 3 and 40 characters.", [str, Length(min=3, max=40)]),
+    ),
+    Required("team_password"): check(
+        ("Passwords must be between 3 and 20 characters.", [str, Length(min=3, max=20)]))
+}, extra=True)
 
 @blueprint.route('/join', methods=['POST'])
 @api_wrapper
 @require_login
 def join_team_hook():
-    api.team.join_team_request(api.common.flat_multi(request.form))
+
+    params = api.common.flat_multi(request.form)
+    validate(join_team_schema, params)
+
+    api.team.join_team(params["team_name"], params["team_password"])
+
     return WebSuccess("You have successfully joined that team!")
 
 @blueprint.route("/settings")
--- api/team.py
+++ api/team.py
@@ -8,27 +8,6 @@ from api.common import safe_fail, WebException, InternalException, SevereInterna
 
 from api.annotations import log_action
 from api.common import check, validate, safe_fail
-from voluptuous import Required, Length, Schema
-
-new_team_schema = Schema({
-    Required("team_name"): check(
-        ("The team name must be between 3 and 40 characters.", [str, Length(min=3, max=40)]),
-        ("A team with that name already exists.", [
-            lambda name: safe_fail(api.team.get_team, name=name) is None]),
-        ("A username with that name already exists.", [
-            lambda name: safe_fail(api.user.get_user, name=name) is None]),
-    ),
-    Required("team_password"): check(
-        ("Passwords must be between 3 and 20 characters.", [str, Length(min=3, max=20)]))
-}, extra=True)
-
-join_team_schema = Schema({
-    Required("team_name"): check(
-        ("The team name must be between 3 and 40 characters.", [str, Length(min=3, max=40)]),
-    ),
-    Required("team_password"): check(
-        ("Passwords must be between 3 and 20 characters.", [str, Length(min=3, max=20)]))
-}, extra=True)
 
 def get_team(tid=None, name=None):
     """
@@ -99,35 +78,6 @@ def get_groups(tid=None, uid=None):
                        'score': api.stats.get_group_average_score(gid=group['gid'])})
 
     return groups
-
-def create_new_team_request(params, uid=None):
-    """
-    Fulfills new team requests for users who have already registered.
-
-    Args:
-        team_name: The desired name for the team. Must be unique across users and teams.
-        team_password: The team's password.
-    Returns:
-        True if successful, exception thrown elsewise. 
-    """
-
-    validate(new_team_schema, params)
-
-    user = api.user.get_user(uid=uid)
-    current_team = api.team.get_team(tid=user["tid"])
-
-    if current_team["team_name"] != user["username"]:
-        raise InternalException("You can only create one new team per user account!")
-
-    desired_tid = create_team({
-        "team_name": params["team_name"],
-        "password": params["team_password"],
-        # The team's affiliation becomes the creator's affiliation.
-        "affiliation": current_team["affiliation"],
-        "eligible": True
-    })
-
-    return join_team(params["team_name"], params["team_password"], user["uid"])
 
 def create_team(params):
     """
@@ -247,19 +197,6 @@ def get_all_teams(ineligible=False, eligible=True, show_ineligible=False):
 
     db = api.common.get_conn()
     return list(db.teams.find(match, {"_id": 0}))
-
-def join_team_request(params):
-    """
-    Validate and process a join_team request.
-
-    Args:
-        team_name
-        team_password
-    """
-
-    validate(join_team_schema, params)
-
-    return join_team(params["team_name"], params["team_password"])
 
 def join_team(team_name, password, uid=None):
     """
